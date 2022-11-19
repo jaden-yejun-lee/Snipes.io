28bbf9d5d100ebd613d1ca0e076f4dae24409ba0
Ingrid Lee <lee.ingrid2003@gmail.com>
2022-11-18 18:23:52 -0800

    updated users.js to work for now

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,6 +1,19 @@
 const mongoose = require('mongoose')
 
-//The schema model for Sale
+//reference: https://stackoverflow.com/questions/55878496/mongoose-populate-on-two-dimensional-array
+//schema model for history
+const historySchema = new mongoose.Schema({
+    gameID: {
+        type: String,
+        required: false
+    },
+    points: {
+        type: Number,
+        required: false
+    },
+})
+
+//The schema model for users
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -29,19 +42,7 @@ const userSchema = new mongoose.Schema({
 versionKey: false
 })
 
-//reference: https://stackoverflow.com/questions/55878496/mongoose-populate-on-two-dimensional-array
-const historySchema = new mongoose.Schema({
-    gameID: {
-        type: String,
-        required: true
-    },
-    points: {
-        type: Number,
-        required: true
-    },
-})
-
 //Exporting the model that associates the User collection with sale schema
 //NOTE: if you change 'Sale' to something else, it will try to access a different collection in the DB
 module.exports = mongoose.model('users', userSchema)
-module.exports(mongoose.model('userHistory', historySchema))
+module.exports = mongoose.model('userHistory', historySchema)
